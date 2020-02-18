f54b4612538ebcc2164abb64d54e81c5f60a54e4
soheon-lee <lsheon93@gmail.com>
2020-02-18 23:27:48 +0900

    Seperate allergy from product

--- starbucks_menu/models.py
+++ starbucks_menu/models.py
@@ -4,39 +4,47 @@ class MainCategory(models.Model):
     name = models.CharField(max_length = 45)
 
     class Meta:
-        db_table = 'maincategories' # main_categories
+        db_table = 'main_categories'
 
 class SubCategory(models.Model):
     main_category = models.ForeignKey(MainCategory, on_delete = models.CASCADE)
     name     = models.CharField(max_length = 45)
 
     class Meta:
-        db_table = 'subcategories'
+        db_table = 'sub_categories'
 
 class Product(models.Model):
-    main_category        = models.ForeignKey(MainCategory, on_delete = models.CASCADE)    
-    sub_category         = models.ForeignKey(SubCategory, on_delete = models.CASCADE)
-    big_image_url        = models.URLField(max_length = 500)
-    name                 = models.CharField(max_length = 45)
-    english_name         = models.CharField(max_length = 300)
-    main_comment         = models.CharField(max_length = 550)
-    calories             = models.DecimalField(max_digits = 4, decimal_places = 1)
-    fat                  = models.DecimalField(max_digits = 4, decimal_places = 1)
-    protein              = models.DecimalField(max_digits = 4, decimal_places = 1)
-    salt                 = models.DecimalField(max_digits = 4, decimal_places = 1)
-    sugar                = models.DecimalField(max_digits = 4, decimal_places = 1)
-    caffein              = models.DecimalField(max_digits = 4, decimal_places = 1)
-    small_image_url      = models.URLField(max_length = 500)
-    sub_comment          = models.CharField(max_length = 500)
+    main_category       = models.ForeignKey(MainCategory, on_delete = models.CASCADE)    
+    sub_category        = models.ForeignKey(SubCategory, on_delete = models.CASCADE)
+    big_image_url       = models.URLField(max_length = 500, null=True)
+    name                = models.CharField(max_length = 45, null=True)
+    english_name        = models.CharField(max_length = 300, null=True)
+    main_comment        = models.CharField(max_length = 550, null=True)
+    calories            = models.DecimalField(max_digits = 4, decimal_places = 1, null=True)
+    fat                 = models.DecimalField(max_digits = 4, decimal_places = 1, null=True)
+    protein             = models.DecimalField(max_digits = 4, decimal_places = 1, null=True)
+    salt                = models.DecimalField(max_digits = 4, decimal_places = 1, null=True)
+    sugar               = models.DecimalField(max_digits = 4, decimal_places = 1, null=True)
+    caffein             = models.DecimalField(max_digits = 4, decimal_places = 1, null=True)
+    small_image_url     = models.URLField(max_length = 500, null=True)
+    sub_comment         = models.CharField(max_length = 500, null=True)
+    product_allergy     = models.ManyToManyField('Allergy', through='ProductAllergy', null=True)
+    product_allergy2    = models.ManyToManyField('Allergy2', null=True)
 
     class Meta:
         db_table = 'products'
 
 class Allergy(models.Model):
-    ingredient = models.CharField(max_length = 45)
+    name = models.CharField(max_length = 45)
 
     class Meta:
         db_table = 'allergies'
+
+class Allergy2(models.Model):
+    name = models.CharField(max_length = 45)
+
+    class Meta:
+        db_table = 'allergies2'
 
 class ProductAllergy(models.Model):
     product = models.ForeignKey(Product, on_delete = models.CASCADE)
